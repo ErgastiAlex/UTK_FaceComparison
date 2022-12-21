32e936aadb9669b4a05c1318784ebd10973f6210
DESKTOP-BD5OQR9\Alex <ergasti.alex@gmail.com>
2022-12-21 11:07:07 +0100

    Separate from main.py the utility functions

---
+++ Utility/utility.py
@@ -0,0 +1,69 @@
+import torch
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+
+def display_dataset_info(dataset, nrow, n_images,prefix=""):
+    """
+    Display some images from the dataset and distribution of the ages in the dataset
+    """
+
+    writer=SummaryWriter(f'./runs/{prefix}_images')
+    writer.add_text(f"{prefix} dataset size", str(len(dataset)))
+
+
+    show_dataset_image_example(dataset, nrow, n_images, writer)
+    show_dataset_statistics(dataset, writer)
+
+
+    writer.close()
+
+def show_dataset_statistics(dataset, writer):
+    """
+    Display the distribution of the ages and the ages difference
+    """
+
+    # Get the ages and the ages difference and display them
+    ages=np.array(dataset.get_ages())
+    ages_diff=np.array(dataset.get_ages_diff())
+
+    writer.add_histogram('Ages', ages, bins="auto")
+    writer.add_histogram('Ages difference', ages_diff, bins="auto")
+
+def show_dataset_image_example(dataset, nrow, n_images, writer):
+    """
+    Display some images from the dataset
+    """
+    images=[]
+    indexes=torch.randint(len(dataset), size=(n_images,)).tolist()
+    for i in indexes:
+        images.append(dataset[i][0])
+
+    images=torch.stack(images) # [x, 2, 3, 224, 224]
+
+    images_1,images_2=torch.split(images,1,1) # [x, 2, 3, 224, 224] -> [x, 1, 3, 224, 224] [x, 1, 3, 224, 224]
+    images_1=images_1.squeeze(1) # [x, 1, 3, 224, 224] -> [x, 3, 224, 224]
+    images_2=images_2.squeeze(1) # [x, 1, 3, 224, 224] -> [x, 3, 224, 224]
+
+    images=torch.cat([images_1,images_2],3) # [x, 3, 224, 224] [x, 3, 224, 224] -> [x, 3, 224, 448]
+
+    # Display the images from the dataset 
+    images = torchvision.utils.make_grid(denorm(images[:n_images]), padding=10,nrow=nrow)
+    writer.add_image('Images', images)
+
+    writer.flush()
+
+def add_model_info_to_tensorboard(writer, args, model):
+    """
+    Add args and graph of the model to tensorboard
+    """
+    
+    writer.add_text("Training info", "Training info: \r" + str(args))
+    writer.add_graph(model,torch.rand(1,2,3,224,224))
+
+    writer.flush()
+
+
+def denorm(x):
+    out = (x + 1) / 2
+    return out.clamp_(0, 1)
--- main.py
+++ main.py
@@ -2,42 +2,56 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import argparse
+
+
 from torch.utils.tensorboard import SummaryWriter
 #TODO Fix this
 from Dataset.UTKDataset import UTKDataset
 from Solver.Solver import Solver
 from torch.utils.data import DataLoader
 from Model.SiameseResNet import SiameseResNet
+import Utility.utility as utility
 
+import numpy as np
 
 def get_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--run_name', type=str,
-                        default="run_1", help='name of current run')
+                        default="exp1", help='name of current run')
 
+    # Training parameters
     parser.add_argument('--epochs', type=int, default=2, help='number of epochs')
     parser.add_argument('--batch_size', type=int, default=16, help='number of elements in batch size')
 
     parser.add_argument('--workers', type=int, default=1, help='number of workers in data loader')
     parser.add_argument('--print_every', type=int, default=500, help='print losses every N iteration')
 
-    parser.add_argument('--lr', type=int, default=0.001, help='learning rate')
 
+    # Model parameters
+    parser.add_argument('--model', type=str, default='SiameseResNet', choices=['SiameseResNet'], help='model used')
+    parser.add_argument('--lr', type=int, default=0.001, help='learning rate')
     parser.add_argument('--opt', type=str, default='Adam', choices=['SGD', 'Adam'], help = 'optimizer used for training')
     parser.add_argument('--criterion', type=str, default='BCELoss', choices=['BCELoss', 'MSELoss'], help = 'criterion used for training')
 
-    # parser.add_argument('--use_norm', action='store_true', help='use normalization layers in model')
-    # parser.add_argument('--feat', type=int, default=16, help='number of features in model')
+    #TODO implement automatic hyperparameter tuning
+    # Automatic hyperparameter tuning
+    parser.add_argument('--autotune', action='store_true', help='enable automatic hyperparameter tuning')
+
+    parser.add_argument('--disable_norm', action='store_true', help="disable normalization of the images")
 
 
+    # Path parameters
     parser.add_argument('--dataset_path', type=str, default='./UTKFace', help='path were to get the dataset')
     parser.add_argument('--checkpoint_path', type=str, default='./models', help='path were to save the trained model')
 
+
     parser.add_argument('--resume_train', action='store_true', help='load the model from checkpoint before training')
+
+    # Dataset parameters
     parser.add_argument('--seed', type=int, default=42, help='seed for the random number generator')
     parser.add_argument('--year_diff', type=int, default=1, help='minimum age difference between the two images')
-    parser.add_argument('--train_size', type=int, default=1000, help='number of images to use to generate the training dataset. If it is greater than the number of images in the directory, it will be clamped to the number of images in the directory')
+    parser.add_argument('--train_size', type=int, default=500, help='number of images to use to generate the training dataset. If it is greater than the number of images in the directory, it will be clamped to the number of images in the directory')
     parser.add_argument('--test_size', type=int, default=100, help='number of images to use to generate the test dataset.')
 
     return parser.parse_args()
@@ -46,24 +60,58 @@ def get_args():
 def main(args):
     writer = SummaryWriter('./runs/' + args.run_name)
 
-    # Load the dataset
-    transform = transforms.Compose(
-        [transforms.Resize((224,224)),
-            transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
+    transform=get_transform(args.disable_norm)
+
+    # Load the dataset 
     train_dataset=UTKDataset(root_dir=args.dataset_path, transform=transform, seed=args.seed, year_diff=args.year_diff, data_size=args.train_size)
     test_dataset=UTKDataset(root_dir=args.dataset_path, transform=transform, seed=args.seed, year_diff=args.year_diff, data_size=args.test_size,exclude_images=train_dataset.get_images())
 
+    utility.display_dataset_info(train_dataset, 5, 10,prefix="train")
+    utility.display_dataset_info(test_dataset, 5, 10,prefix="test")
+
+    
+    # Create the dataloaders
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
     test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers)
+
 
     device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("Using device: ")
     print(device)
 
-    solver= Solver(train_loader,test_loader,device,SiameseResNet(),writer,args)
 
-    solver.train()
+    model=choose_model(args)
+
+    if args.autotune==False:
+        utility.add_model_info_to_tensorboard(writer, args, model)
+
+        solver= Solver(train_loader,test_loader,device,SiameseResNet(),writer,args)
+
+        solver.train()
+
+
+def get_transform(disalbe_norm):
+    if disalbe_norm:
+        return transforms.Compose([
+            transforms.Resize((224,224)),
+            transforms.ToTensor()
+        ])
+    else:
+        return transforms.Compose([
+            transforms.Resize((224,224)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
+        ])
+
+
+
+def choose_model(args):
+    if args.model == 'SiameseResNet':
+        return SiameseResNet()
+
+
+
 
 if __name__ == "__main__":
     args = get_args()
